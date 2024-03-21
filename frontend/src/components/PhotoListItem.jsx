05ae5fbe5d1e4cc3e73d1fa36e9aeb9a417e764f
TapiocaQueen990 <daniel.phou@hotmail.com>
2024-03-20 22:47:21 -0400

    implemented changes as suggested by submission AI

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -10,9 +10,11 @@ const PhotoListItem = ({ sampleDataForPhotoListItem }) => {
   return (
     <div className="photo-list__item">
     <img className="photo-list__image" src={sampleDataForPhotoListItem.imageSource} alt="image" />
+    <div className="photo-list__user-details">
     <img className="photo-list__user-profile" src={sampleDataForPhotoListItem.profile} alt="profile" />
     <p className="photo-list__user-info">{sampleDataForPhotoListItem.username}</p>
     <p className="photo-list__user-location">{location.city}, {location.country}</p>
+    </div>
     </div>
   )
 };
