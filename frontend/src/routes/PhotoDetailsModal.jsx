53e0874a464d9d5bfcbbd9f34493b4139d029e4d
TapiocaQueen990 <daniel.phou@hotmail.com>
2024-03-25 21:46:55 -0400

    connected favorite button to modal view

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -5,6 +5,7 @@ import closeSymbol from '../assets/closeSymbol.svg';
 import PhotoList from 'components/PhotoList';
 import PhotoFavButton from 'components/PhotoFavButton';
 import ModalPhotoList from 'components/ModalPhotoList';
+import photos from 'mocks/photos';
 
 
 
@@ -18,32 +19,56 @@ const PhotoDetailsModal = (props) => {
       }
     }
   }
+    
     const faveThisPic=props.toggleFave
     const chosenPic = chosenPhoto()
-    console.log(chosenPic);
+    const chosenPicObject = chosenPic.similar_photos
+    const similarArr = []
+    for (const key in chosenPicObject) {
+      similarArr.push(chosenPicObject[key])
+    }
+    console.log(similarArr, "similar photos");
+
+    
+   
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={props.toggleM}>
+      <button
+        className="photo-details-modal__close-button"
+        onClick={props.toggleM}
+      >
         <img src={closeSymbol} alt="close symbol" />
       </button>
+      <div className="photo-details-modal__images">
+        <PhotoFavButton fave={props.fave} toggleFave={props.toggleFave} />
+        <img className="photo-details-modal__image" src={chosenPic.urls.full} />
 
-      <div className='photo-details-modal__images'>
-      <PhotoFavButton id={chosenPic.id} fave={props.fave} toggleFave={props.toggleFave}/>
-        <img className='photo-details-modal__image' src={chosenPic.urls.full} alt={chosenPic.id + " picture"}/>
-    
-      <div>
-        <img className='photo-details-modal__photographer-profile' src={chosenPic.user.profile}/>
-        <div className='photo-details-modal__photographer-info'>{chosenPic.user.username}</div>
-        <div className='photo-details-modal__photographer-location'>{chosenPic.location.city}, {chosenPic.location.country}</div>
+        <div className="photo-details-modal__photographer-details">
+          <img
+            className="photo-details-modal__photographer-profile"
+            src={chosenPic.user.profile}
+          />
+          <div className="photo-list__user-profile">
+            <span className="photo-details-modal__photographer-info">
+              {chosenPic.user.username}
+            </span>
+            <div className="photo-details-modal__photographer-location">
+            {chosenPic.location.city},{chosenPic.location.country}
+            </div>
+          </div>
+        </div>
       </div>
-      <h2 className='photo-details-modal__header'>Similar Photos</h2>
+      <h2 className="photo-details-modal__header">Similar Photos</h2>
+
+      <div className="photo-details-modal__image">
+        <PhotoList
+          photos={similarArr}
+          toggleFave={props.toggleFave}
+          fave={props.fave}
+        />
       </div>
-      
-      <div className='photo-details-modal__image'><PhotoList photos={props.photos} fave={props.fave} toggleFave={props.toggleFave}/>
-      </div>
-      
     </div>
-  )
+  );
 };
 
 export default PhotoDetailsModal;
